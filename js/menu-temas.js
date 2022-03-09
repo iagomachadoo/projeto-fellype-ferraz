fc9b6d94da77faef0b75253fe5bf20accb92bdf3
Iago MAchado <iagomachado22@gmail.com>
2022-03-09 10:09:12 -0300

    Refatorando o código para diminuir a quantidade de estilos css.

--- js/menu-temas.js
+++ js/menu-temas.js
@@ -1,29 +1,35 @@
-const headerItem = document.querySelector('.header__item--relative');
-
-const headerLinkSeta = headerItem.querySelector('.header__link--seta')
-
-headerLinkSeta.addEventListener('click', abreFechaTemas);
-
-headerLinkSeta.addEventListener('touchstart', abreFechaTemas);
-
-function abreFechaTemas(evento) {
-    if(evento.type === 'touchstart'){
-        evento.preventDefault()
-    }
-
-    const subMenuLista = document.querySelector('.sub-menu__lista');
-
-    subMenuLista.classList.toggle('is-ativo');
-
-    const subMenuAtivo = subMenuLista.classList.contains('is-ativo')
-
-    evento.currentTarget.setAttribute('aria-expanded', subMenuAtivo)
-
-    if (subMenuAtivo) {
-        headerLinkSeta.classList.toggle( 'header__link--seta-180-deg')
-        evento.currentTarget.setAttribute('aria-label', 'Fechar temas')
-    } else {
-        headerLinkSeta.classList.remove( 'header__link--seta-180-deg')
-        evento.currentTarget.setAttribute('aria-label',  'Abrir temas')
+function initMenuTemas() {
+    const headerItem = document.querySelector('.header__item--relative');
+    
+    if (headerItem) {
+        const headerLinkSeta = headerItem.querySelector('.header__btn--seta')
+        
+        headerLinkSeta.addEventListener('click', abreFechaTemas);
+        
+        headerLinkSeta.addEventListener('touchstart', abreFechaTemas);
+        
+        function abreFechaTemas(evento) {
+            if(evento.type === 'touchstart'){
+                evento.preventDefault()
+            }
+        
+            const subMenuLista = document.querySelector('.sub-menu__lista');
+        
+            subMenuLista.classList.toggle('is-ativo');
+        
+            const subMenuAtivo = subMenuLista.classList.contains('is-ativo')
+        
+            evento.currentTarget.setAttribute('aria-expanded', subMenuAtivo)
+        
+            if (subMenuAtivo) {
+                headerLinkSeta.classList.toggle( 'header__btn--seta-180-deg')
+                evento.currentTarget.setAttribute('aria-label', 'Fechar temas')
+            } else {
+                headerLinkSeta.classList.remove( 'header__btn--seta-180-deg')
+                evento.currentTarget.setAttribute('aria-label',  'Abrir temas')
+            }
+        }
     }
 }
+
+initMenuTemas();
--- js/perguntas.js
+++ js/perguntas.js
@@ -1,27 +1,34 @@
-const pergunta = document.querySelectorAll('.perguntas__pergunta');
 
-pergunta.forEach((btn) => {
-    btn.addEventListener('click', abriFechaTexto)
-})
-
-function abriFechaTexto(el) {
-    const btnClicado = el.currentTarget;
-
-    const ariaControlsBtnCLicado = btnClicado.getAttribute('aria-controls');
-
-    const ariaExpandedBtnCLicado = btnClicado.getAttribute('aria-expanded');
-
-    const resposta = document.getElementById(ariaControlsBtnCLicado);
-
-    resposta.classList.toggle('is-ativo');
-
-    btnClicado.classList.toggle('perguntas__pergunta--rotacionar');
-
-    const contemClasseAtivo = resposta.classList.contains('is-ativo');
-
-    if (contemClasseAtivo) {
-        btnClicado.setAttribute('aria-expanded', contemClasseAtivo)
-    }else{
-        btnClicado.setAttribute('aria-expanded', contemClasseAtivo)
+function initPergunta() {
+    const pergunta = document.querySelectorAll('.perguntas__pergunta');
+    
+    if (pergunta) {
+        pergunta.forEach((btn) => {
+            btn.addEventListener('click', abriFechaTexto)
+        })
+        
+        function abriFechaTexto(el) {
+            const btnClicado = el.currentTarget;
+        
+            const ariaControlsBtnCLicado = btnClicado.getAttribute('aria-controls');
+        
+            const ariaExpandedBtnCLicado = btnClicado.getAttribute('aria-expanded');
+        
+            const resposta = document.getElementById(ariaControlsBtnCLicado);
+        
+            resposta.classList.toggle('is-ativo');
+        
+            btnClicado.classList.toggle('perguntas__pergunta--rotacionar');
+        
+            const contemClasseAtivo = resposta.classList.contains('is-ativo');
+        
+            if (contemClasseAtivo) {
+                btnClicado.setAttribute('aria-expanded', contemClasseAtivo)
+            }else{
+                btnClicado.setAttribute('aria-expanded', contemClasseAtivo)
+            }
+        }
     }
 }
+
+initPergunta();
