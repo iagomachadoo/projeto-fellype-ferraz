6b7570fa9ca8b3f28239903116b0dbe06249a0ce
Iago MAchado <iagomachado22@gmail.com>
2022-03-14 14:24:47 -0300

    Retirando o decorador de indicação de página atual para telas menores que 600px.

--- js/pagina-atual.js
+++ js/pagina-atual.js
@@ -1,13 +1,26 @@
-const links = document.querySelectorAll('.header__link');
-
-links.forEach(ativarLink)
-
-function ativarLink(link) {
-    const hrefLinkClicado = link.href;
-
-    const linkPaginaAtual = location.href;
-
-    if (linkPaginaAtual.includes(hrefLinkClicado)) {
-        link.setAttribute('class', 'link-ativo')
-    }   
+function indicarPaginaAtual(){
+    const links = document.querySelectorAll('.header__link');
+    
+    const larguraDaJanelaDoNavegador = window.innerWidth
+    
+    if(links){
+        if(larguraDaJanelaDoNavegador > 600){
+            links.forEach(ativarLink)
+        
+            function ativarLink(link) {
+                const hrefLinkClicado = link.href;
+        
+                const linkPaginaAtual = location.href;
+        
+                if (linkPaginaAtual.includes(hrefLinkClicado)) {
+                    link.setAttribute('class', 'link-ativo')
+                }   
+            }
+    
+        } else{
+            console.log('janela mobile.');
+        }
+    }
 }
+
+indicarPaginaAtual()
